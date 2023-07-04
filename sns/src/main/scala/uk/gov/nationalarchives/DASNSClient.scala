d42c6ed00297a4c147381f34620b84c319dc4c40
techncl <nigel.lutale@nationalarchives.gov.uk>
2023-07-04 11:32:23 +0100

    Replace 'publish' with 'publishBatch' to reduce cost

--- sns/src/main/scala/uk/gov/nationalarchives/DASNSClient.scala
+++ sns/src/main/scala/uk/gov/nationalarchives/DASNSClient.scala
@@ -1,43 +1,62 @@
 package uk.gov.nationalarchives
 
 import cats.effect.Async
+import cats.implicits._
 import io.circe.syntax._
 import io.circe.{Encoder, Printer}
 import software.amazon.awssdk.http.async.SdkAsyncHttpClient
 import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.sns.SnsAsyncClient
-import software.amazon.awssdk.services.sns.model.{PublishRequest, PublishResponse}
+import software.amazon.awssdk.services.sns.model.{PublishBatchRequest, PublishBatchRequestEntry, PublishBatchResponse}
+
+import java.util
+import scala.jdk.CollectionConverters.SeqHasAsJava
 
 /** An SNS client. It is written generically so can be used for any effect which has an Async instance. Requires an
- * implicit instance of cats Async which is used to convert CompletableFuture to F
- *
- * @param snsAsyncClient
- * An AWS SNS Async client
- * @tparam F
- * Type of the effect
- */
-class DASNSClient[F[_] : Async](snsAsyncClient: SnsAsyncClient) {
+  * implicit instance of cats Async which is used to convert CompletableFuture to F
+  *
+  * @param snsAsyncClient
+  *   An AWS SNS Async client
+  * @tparam F
+  *   Type of the effect
+  */
+class DASNSClient[F[_]: Async](snsAsyncClient: SnsAsyncClient) {
 
   /** Deserialises the provided value to JSON and sends to the provided SNS topic.
-   *
-   * @param topicArn
-   * The arn for the SNS topic
-   * @param message
-   * A case class which will be deserialised to JSON and sent to the topic or a json string
-   * @param enc
-   * A circe encoder which will encode the case class to JSON
-   * @tparam T
-   * The case class with the message
-   * @return
-   * The response from the send message call wrapped with F[_]
-   */
-  def publish[T <: Product](topicArn: String)(message: T)(implicit enc: Encoder[T]): F[PublishResponse] = {
-    val messageRequest = PublishRequest.builder
-      .topicArn(topicArn)
-      .message(message.asJson.printWith(Printer.noSpaces))
-      .build()
-    Async[F].fromCompletableFuture(Async[F].pure(snsAsyncClient.publish(messageRequest)))
+    *
+    * @param topicArn
+    *   The arn for the SNS topic
+    * @param messages
+    *   A List of case classes which will be deserialised to JSON and sent to the topic or a json string
+    * @param enc
+    *   A circe encoder which will encode the case class to JSON
+    * @tparam T
+    *   The case class with the message
+    * @return
+    *   The response from the send message call wrapped with F[_]
+    */
+  def publish[T <: Product](
+      topicArn: String
+  )(messages: List[T])(implicit enc: Encoder[T]): F[List[PublishBatchResponse]] = {
+    val batchesOfTenMessages: List[List[T]] = messages.grouped(10).toList
+
+    batchesOfTenMessages.map { batchOfTenMessages =>
+      val batchOfTenJsonMessages: util.List[PublishBatchRequestEntry] = batchOfTenMessages.map { message =>
+        val messageAsJson: String = message.asJson.printWith(Printer.noSpaces)
+        PublishBatchRequestEntry
+          .builder()
+          .message(messageAsJson)
+          .build()
+      }.asJava
+
+      val batchMessageRequest: PublishBatchRequest =
+        PublishBatchRequest.builder
+          .topicArn(topicArn)
+          .publishBatchRequestEntries(batchOfTenJsonMessages)
+          .build()
+      Async[F].fromCompletableFuture(Async[F].pure(snsAsyncClient.publishBatch(batchMessageRequest)))
+    }.sequence
   }
 }
 
@@ -49,5 +68,5 @@ object DASNSClient {
     .httpClient(httpClient)
     .build()
 
-  def apply[F[_] : Async]() = new DASNSClient[F](snsClient)
+  def apply[F[_]: Async]() = new DASNSClient[F](snsClient)
 }
--- sns/src/test/scala/uk/gov/nationalarchives/DASNSClientTest.scala
+++ sns/src/test/scala/uk/gov/nationalarchives/DASNSClientTest.scala
@@ -8,9 +8,11 @@ import org.mockito.{ArgumentCaptor, MockitoSugar}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 import software.amazon.awssdk.services.sns.SnsAsyncClient
-import software.amazon.awssdk.services.sns.model.{PublishRequest, PublishResponse}
+import software.amazon.awssdk.services.sns.model.{PublishBatchRequest, PublishBatchRequestEntry, PublishBatchResponse}
 
+import java.util
 import java.util.concurrent.CompletableFuture
+import scala.jdk.CollectionConverters.ListHasAsScala
 
 class DASNSClientTest extends AnyFlatSpec with MockitoSugar {
 
@@ -18,30 +20,72 @@ class DASNSClientTest extends AnyFlatSpec with MockitoSugar {
 
   implicit val enc: Encoder[Test] = Encoder.forProduct2("message", "value")(t => (t.message, t.value))
 
-  "publish" should "send the correct message to the topic" in {
+  "publishBatch" should "send the correct messages to the topic" in {
     val snsAsyncClient = mock[SnsAsyncClient]
-    val publishCaptor: ArgumentCaptor[PublishRequest] = ArgumentCaptor.forClass(classOf[PublishRequest])
-    val mockResponse = CompletableFuture.completedFuture(PublishResponse.builder().build())
-    when(snsAsyncClient.publish(publishCaptor.capture())).thenReturn(mockResponse)
+    val publishCaptor: ArgumentCaptor[PublishBatchRequest] = ArgumentCaptor.forClass(classOf[PublishBatchRequest])
+    val mockResponse = CompletableFuture.completedFuture(PublishBatchResponse.builder().build())
+    when(snsAsyncClient.publishBatch(publishCaptor.capture())).thenReturn(mockResponse)
 
     val client = new DASNSClient[IO](snsAsyncClient)
-    client.publish("mockTopicArn")(Test("testMessage", "testValue")).unsafeRunSync()
+    client.publish("mockTopicArn")(List(Test("testMessage1", "testValue1"), Test("testMessage2", "testValue2"))).unsafeRunSync()
 
     val publishValue = publishCaptor.getValue
 
-    publishValue.message() should equal("""{"message":"testMessage","value":"testValue"}""")
+    val batchMessages: List[String] = getMessagesFromBatchRequestEntries(publishValue.publishBatchRequestEntries())
+
+    batchMessages should be(
+      List("""{"message":"testMessage1","value":"testValue1"}""", """{"message":"testMessage2","value":"testValue2"}""")
+    )
+
     publishValue.topicArn() should equal("mockTopicArn")
   }
 
-  "publish" should "return an error if there is an error sending to the topic" in {
+  "publishBatch" should "send up to 10 messages per batch to the topic" in {
     val snsAsyncClient = mock[SnsAsyncClient]
-    when(snsAsyncClient.publish(any[PublishRequest])).thenThrow(new Exception("Error sending message"))
+    val publishCaptor: ArgumentCaptor[PublishBatchRequest] = ArgumentCaptor.forClass(classOf[PublishBatchRequest])
+    val mockResponse = CompletableFuture.completedFuture(PublishBatchResponse.builder().build())
+    when(snsAsyncClient.publishBatch(publishCaptor.capture())).thenReturn(mockResponse)
+
+    val messages = (1 to 23).toList.map(number => Test(s"testMessage$number", s"testValue$number"))
+    val convertNumberToJsonMessage = (number: Int) => s"""{"message":"testMessage$number","value":"testValue$number"}"""
+
+    val client = new DASNSClient[IO](snsAsyncClient)
+    client.publish("mockTopicArn")(messages).unsafeRunSync()
+
+    val publishedBatchRequests: List[PublishBatchRequest] = publishCaptor.getAllValues.asScala.toList
+    val batchRequestEntries1 =  publishedBatchRequests.head.publishBatchRequestEntries()
+    val batchRequestEntries2 =  publishedBatchRequests(1).publishBatchRequestEntries()
+    val batchRequestEntries3 =  publishedBatchRequests(2).publishBatchRequestEntries()
+
+    val batchMessages1: List[String] = getMessagesFromBatchRequestEntries(batchRequestEntries1)
+    val batchMessages2: List[String] = getMessagesFromBatchRequestEntries(batchRequestEntries2)
+    val batchMessages3: List[String] = getMessagesFromBatchRequestEntries(batchRequestEntries3)
+
+    publishedBatchRequests.length should be(3)
+    batchRequestEntries1.size() should be(10)
+    batchMessages1 should be((1 to 10).toList.map(convertNumberToJsonMessage))
+
+    batchRequestEntries2.size() should be(10)
+    batchMessages2 should be((11 to 20).toList.map(convertNumberToJsonMessage))
+
+    batchRequestEntries3.size() should be(3)
+    batchMessages3 should be((21 to 23).toList.map(convertNumberToJsonMessage))
+  }
+
+  "publishBatch" should "return an error if there is an error sending to the topic" in {
+    val snsAsyncClient = mock[SnsAsyncClient]
+    when(snsAsyncClient.publishBatch(any[PublishBatchRequest])).thenThrow(new Exception("Error sending message"))
 
     val client = new DASNSClient[IO](snsAsyncClient)
 
     val ex = intercept[Exception] {
-      client.publish("mockTopicArn")(Test("testMessage", "testValue")).unsafeRunSync()
+      client.publish("mockTopicArn")(List(Test("testMessage1", "testValue1"), Test("testMessage2", "testValue2"))).unsafeRunSync()
     }
     ex.getMessage should equal("Error sending message")
+  }
+
+  private def getMessagesFromBatchRequestEntries(batchRequestEntry: util.List[PublishBatchRequestEntry]): List[String] = {
+    val batchEntries: List[PublishBatchRequestEntry] = batchRequestEntry.asScala.toList
+    batchEntries.map(_.message())
   }
 }
