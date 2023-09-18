7ae7c5bc882e3327c6f668331ee0f299343b545c
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-09-18 12:53:38 +0000

    Update scanamo to 1.0.0-M28

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,12 +11,12 @@ object Dependencies {
   lazy val circe = "io.circe" %% "circe-core" % circeVersion
   lazy val dynamoDB = "software.amazon.awssdk" % "dynamodb" % awsSdkVersion
   lazy val eventBridgeSdk = "software.amazon.awssdk" % "eventbridge" % awsSdkVersion
-  lazy val log4Cats =  "org.typelevel" %% "log4cats-slf4j"   % "2.6.0"
+  lazy val log4Cats = "org.typelevel" %% "log4cats-slf4j" % "2.6.0"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.12"
   lazy val reactorTest = "io.projectreactor" % "reactor-test" % "3.5.6"
   lazy val s3Sdk = "software.amazon.awssdk" % "s3" % awsSdkVersion
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
-  lazy val scanamo = "org.scanamo" %% "scanamo" % "1.0-M13"
+  lazy val scanamo = "org.scanamo" %% "scanamo" % "1.0.0-M28"
   lazy val snsSdk = "software.amazon.awssdk" % "sns" % awsSdkVersion
   lazy val sfnSdk = "software.amazon.awssdk" % "sfn" % awsSdkVersion
   lazy val sqsSdk = "software.amazon.awssdk" % "sqs" % awsSdkVersion
