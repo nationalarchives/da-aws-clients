b517539849c845ad0b638eef3a4235b540947f92
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-04-10 08:21:23 +0000

    Update reactor-test to 3.6.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   lazy val dynamoDB = "software.amazon.awssdk" % "dynamodb" % awsSdkVersion
   lazy val eventBridgeSdk = "software.amazon.awssdk" % "eventbridge" % awsSdkVersion
   lazy val mockito = "org.scalatestplus" %% "mockito-5-10" % s"$scalaTestVersion.0"
-  lazy val reactorTest = "io.projectreactor" % "reactor-test" % "3.6.4"
+  lazy val reactorTest = "io.projectreactor" % "reactor-test" % "3.6.5"
   lazy val s3Sdk = "software.amazon.awssdk" % "s3" % awsSdkVersion
   lazy val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
   lazy val scanamo = "org.scanamo" %% "scanamo" % "1.0.4"
