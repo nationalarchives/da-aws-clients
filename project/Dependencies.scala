df951aaa11dde0278b77b40347edd95ab4d29963
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-06-19 08:25:19 +0000

    Update circe-core, circe-generic, ... to 0.14.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private val awsSdkVersion = "2.26.3"
-  private val circeVersion = "0.14.7"
+  private val circeVersion = "0.14.8"
   private lazy val scalaTestVersion = "3.2.18"
 
   lazy val awsCrt = "software.amazon.awssdk.crt" % "aws-crt" % "0.29.17"
