7024de48774a93accee1a96e8bd942fe3a5e0377
Sam Palmer <sam.palmer@nationalarchives.gov.uk>
2026-07-09 11:59:55 +0100

    Version bump to fix broken ingest (#675)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  private val awsSdkVersion = "2.46.19"
+  private val awsSdkVersion = "2.47.3"
   private val circeVersion = "0.14.16"
   private lazy val nettyVersion = "4.2.15.Final"
   private lazy val scalaTestVersion = "3.2.20"
